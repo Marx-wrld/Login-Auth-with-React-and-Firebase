6785ccd942824e72c01d0de156cf45920a26815b
Marx-wrld <iongonga@kabarak.ac.ke>
2022-11-30 16:53:11 +0300

    Adding the login, logout, resetpassword functions

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -8,26 +8,52 @@ export function useAuth(){
 }
 
 export function AuthProvider({ children }) {
-    const [currentUser, setCurrentUser] = useState() /* setting the current user to be actually set to the current user using firebase*/
+    const [currentUser, setCurrentUser] = useState()
+    const [loading, setLoading] = useState(true) /* setting the current user to be actually set to the current user using firebase*/
     
     //we are going to use our auth module in firebase to login our user
     function signup(email, password){
         return auth.createUserWithEmailAndPassword(email, password)
     }
+    function login(email, password) {
+        return auth.signInWithEmailAndPassword(email, password)
+      }
+    
+      function logout() {
+        return auth.signOut()
+      }
+    
+      function resetPassword(email) {
+        return auth.sendPasswordResetEmail(email)
+      }
+    
+      function updateEmail(email) {
+        return currentUser.updateEmail(email)
+      }
+    
+      function updatePassword(password) {
+        return currentUser.updatePassword(password)
+      }
     useEffect(() =>{ //this function actually calls a method and when we call this method, it will unsubscribe onAuthStateChanged event.
         const unsubscribe  = auth.onAuthStateChanged(user => {
             setCurrentUser(user)
+            setLoading(false)
         })
 
         return unsubscribe
     }, [])
     const value ={
         currentUser,
-        signup
+        signup,
+        login,
+        logout,
+        resetPassword,
+        updateEmail,
+        updatePassword
     }
 
   return (
-    <AuthContext.Provider value={value}> {/*now accessible anywhere in our application*/}
+    <AuthContext.Provider value={value}> {!loading && children}{/*now accessible anywhere in our application*/}
     </AuthContext.Provider>
   )
 }
