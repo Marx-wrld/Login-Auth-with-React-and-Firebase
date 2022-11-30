5e60a4e360f50e5b1b2d2284fcfbf020b8d433dd
Marx-wrld <iongonga@kabarak.ac.ke>
2022-11-30 13:01:52 +0300

    AuthContext for user access anywhere in the app

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,11 @@ import Signup from "./Signup";
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justif-content-center">
+    <Container className="d-flex align-items-center justif-content-center"
+    style={{ minHeight: "100vh" }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
       <Signup/>
+      </div>
     </Container>
     )
 }
