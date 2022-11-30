77bc3bb45a7517124464cf7a24bb3867d115cd3c
Marx-wrld <iongonga@kabarak.ac.ke>
2022-11-30 16:47:46 +0300

    installing react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from "react"
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 import Signup from "./Signup";
+import Dashboard from "./Dashboard"
+import Login from "./Login"
+import PrivateRoute from "./PrivateRoute"
+import ForgotPassword from "./ForgotPassword"
+import UpdateProfile from "./UpdateProfile"
 
 
 function App() {
@@ -10,7 +16,13 @@ function App() {
     <Container className="d-flex align-items-center justif-content-center"
     style={{ minHeight: "100vh" }}>
       <div className="w-100" style={{ maxWidth: "400px" }}>
-      <Signup/>
+      <Switch>
+              <PrivateRoute exact path="/" component={Dashboard} />
+              <PrivateRoute path="/update-profile" component={UpdateProfile} />
+              <Route path="/signup" component={Signup} />
+              <Route path="/login" component={Login} />
+              <Route path="/forgot-password" component={ForgotPassword} />
+            </Switch>
       </div>
     </Container>
     </AuthProvider>
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import React, { useRef, useState } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext'
+import { Link, useHistory } from "react-router-dom"
 
 export default function Signup() {
     const emailRef = useRef()
@@ -9,7 +10,7 @@ export default function Signup() {
     const { signup } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
-    //const history = useHistory()
+    const history = useHistory()
 
     async function handleSubmit(e){
         e.preventDefault()
@@ -32,7 +33,8 @@ export default function Signup() {
         <Card>
             <Card.Body>
                 <h2 className="text-center mb-4">Register</h2>
-                <Form>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form onSubmit={ handleSubmit }>
                     <Form.Group id="email">   
                         <Form.Label>Email</Form.Label>
                         <Form.Control type="email"
@@ -49,12 +51,12 @@ export default function Signup() {
                         ref={passwordConfirmRef} required/>
                     </Form.Group>
                     <br/>
-                    <Button className='w-100' type="submit">Register</Button>
+                    <Button disabled={loading} className='w-100' type="submit">Register</Button>
                 </Form>
             </Card.Body>
         </Card>
         <div className='w-100 text-center mt-2'>
-            Already have an account? Login
+            Already have an account?  <Link to="/login">Login</Link>
         </div>
     </>
   );
