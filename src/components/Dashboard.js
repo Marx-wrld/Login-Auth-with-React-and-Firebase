8d3c03057c9aede683c080b99f7952a523c2507f
Marx-wrld <iongonga@kabarak.ac.ke>
2022-12-01 14:13:03 +0300

    Dashboard component and logout functionality

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Card, Button, Alert } from 'react-bootstrap'
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Dashboard() {
-  return (
-    <div>
-    Dashboard
+  const { currentUser } = useAuth()
+  const [error, setError] = useState("")
+  function handleLogout() {
+
+  }
+  
+    return (
+    <>
+    <Card>
+        <Card.Body>
+            <h2 className='text-center mb-4'>
+                Profile
+            </h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <strong>Email: </strong> {currentUser.email}
+        </Card.Body>
+    </Card>
+    <div className='w-100 text-center mt-2'>
+        <Button variant="link" onClick={handleLogout}> Log Out</Button>
     </div>
+    </>
   );
 }
