09ab9d6624541ec397425dd1ea8251d216d036aa
Marx-wrld <iongonga@kabarak.ac.ke>
2022-12-06 16:11:55 +0300

    Firebase changes

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,24 +1,16 @@
-import React from 'react'
-import { Route, Navigate, Routes } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { useAuth } from "../contexts/AuthContext"
 
-export default function PrivateRoute({ component : Component, ...rest}) { //wrapper for the current route
-
+export default function PrivateRoute({ component: Component, ...rest }) {
   const { currentUser } = useAuth()
+
   return (
-    <Routes>
     <Route
       {...rest}
       render={props => {
-        return currentUser ? <Component {...props} /> : <Navigate to="/login" />
+        return currentUser ? <Component {...props} /> : <Redirect to="/login" />
       }}
     ></Route>
-    </Routes>
   )
 }
-
-//{/* returning our route*/}
-// {/* This route will take all the rest of the routes*/}
-//here we check if we have a current user
-         //if we have a current user then we just want to render out the component that we got passed into our class.
-         //otherwise we don't want to render this component because its a private component. So, we want to redirect our user
